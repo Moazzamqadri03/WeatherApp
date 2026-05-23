14100cf0b54bb6235c952c6078fcbbfbf0af4149
Moazzamqadri03 <Moazzamqadri03@gmail.com>
2026-05-23 22:36:40 +0530

    Update

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-blue-800'>
-        <p className='text-white w-1/2 mx-auto'>Made with Love <span class="heart">&hearts;</span> by: Uzaib Bashir Malik</p>
+        <p className='text-white w-1/2 mx-auto'>Made with Love <span class="heart">&hearts;</span> by: Moazzam Hussain Qadri</p>
     </div>
   )
 }
